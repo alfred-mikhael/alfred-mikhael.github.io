84daad857e5640668b8ef4e2e9ce893680d3c2e1
Alfred Mikhael <alfred.mikhael@mail.utoronto.ca>
2023-01-01 14:07:56 -0500

    Remove underline and make canvas responsive

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,18 +1,41 @@
-  import React, { useRef, useEffect } from 'react';
+  import React, { useRef, useEffect, useState } from 'react';
   import "./Canvas.css";
 
   const PROBABILITY = 0.2;
 
   const Canvas = props => {
     
-    const { draw, graph, ...rest } = props
-    const canvasRef = useRef(null)
+    const { draw, graph, ...rest } = props;
+    const canvasRef = useRef(null);
+
+    const [dimensions, setDimensions] = useState({
+      width: undefined,
+      height: undefined,
+  });
+
+  useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     useEffect(() => {
       
       const canvas = canvasRef.current
       const context = canvas.getContext('2d')
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight * 80 / 100
+      canvas.width = dimensions["width"];
+      canvas.height = dimensions["height"] * 80 / 100;
       let frameCount = 0
       let animationFrameId
       
