ae0bc92f2f942f1286182e4dc65380dac2256df2
Alfred Mikhael <alfred1.mikhael@gmail.com>
2025-04-27 22:24:43 -0400

    make navbar responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,38 +1,53 @@
 import "./Navbar.css";
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function onToggle() {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div>
-            {/* <div className="graph-container">
-                <GraphAnimation />
-            </div> */}
-
             <nav className='navbar'>
-            <ul>
+                <ul className={`navbar-items ${isOpen ? "open" : ""}`}>
                     <li>
-                        <Link className='nav-item' to="/">
+                        <Link onClick={onToggle} className={`nav-item`} to="/">
                             About
                         </Link>
                     </li>
                     <li>
-                        <Link className="nav-item" to="/experience">
+                        <Link onClick={onToggle} className={`nav-item`} to="/experience">
                             Experience
                         </Link>
                     </li>
                     <li>
-                        <Link className='nav-item' to='/projects'>
+                        <Link onClick={onToggle} className={`nav-item`} to='/projects'>
                             Projects
                         </Link>
                     </li>
                     <li>
-                        <Link className="nav-item" to="/contact">
-                            Contact
+                        <Link onClick={onToggle} className={`nav-item`} to="/research">
+                            Research
+                        </Link>
+                    </li>
+                    <li>
+                        <Link onClick={onToggle} className={`nav-item`} to="/education">
+                            Education
                         </Link>
                     </li>
                 </ul>
+                <button onClick={onToggle} className="hamburger-button">
+                    <div className={`hamburger ${isOpen ? "open" : ""}`}>
+                        <span className={`hamburger-bar ${isOpen ? "open" : ""}`}></span>
+                        <span className={`hamburger-bar middle ${isOpen ? "open" : ""}`}></span>
+                        <span className={`hamburger-bar ${isOpen ? "open" : ""}`}></span>
+                    </div>
+                </button>
             </nav>
         </div>
 
